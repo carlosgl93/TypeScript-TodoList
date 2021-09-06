ba705379e66310ea164bb6b578f23891cf9ca46d
Carlos Gumucio <cgumucio93@gmail.com>
2021-09-06 11:58:09 -0300

    add task functionality and some validations

--- src/screens/ListScreen.tsx
+++ src/screens/ListScreen.tsx
@@ -1,7 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { nanoid } from 'nanoid';
 
 type Props = {};
 
+type Task = {
+  id: string;
+  label: string;
+};
 export const ListScreen: React.FC<Props> = () => {
-  return <div>List View</div>;
+  const [tasks, setTasks] = useState<Task[]>([]);
+  const [newTaskLabel, setNewTaskLabel] = useState('');
+
+  const handleNewTaskLabelChange = (e: React.ChangeEvent<HTMLInputElement>) =>
+    setNewTaskLabel(e.target.value);
+
+  const handleNewTaskKeyPress = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === 'Enter' && newTaskLabel !== '') {
+      setTasks((task) => [...tasks, { id: nanoid(), label: newTaskLabel }]);
+      setNewTaskLabel('');
+    }
+  };
+  return (
+    <div>
+      <ul>
+        {tasks.map((task) => (
+          <li key={task.id}>{task.label}</li>
+        ))}
+      </ul>
+      <input
+        value={newTaskLabel}
+        onChange={handleNewTaskLabelChange}
+        onKeyPress={handleNewTaskKeyPress}
+        type="text"
+      />
+    </div>
+  );
 };
